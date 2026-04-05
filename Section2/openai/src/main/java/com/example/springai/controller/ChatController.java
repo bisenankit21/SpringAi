2295b35ce70d6d79261ed04073d9c4a34f134d06
ankitbisen <bisenankit21@gmail.com>
2026-04-05 13:40:49 +0530

    Initial code

--- Section2/openai/src/main/java/com/example/springai/controller/ChatController.java
+++ Section2/openai/src/main/java/com/example/springai/controller/ChatController.java
@@ -14,12 +14,31 @@ public class ChatController
     private final ChatClient chatClient;
 
     public ChatController(ChatClient.Builder chatClientBuilder){
-        this.chatClient =chatClientBuilder.build();
+        this.chatClient =chatClientBuilder
+                .defaultSystem("""
+                        You are an internal IT helpdesk assistant. Your role is to assist 
+                        employees with IT-related issues such as resetting passwords, 
+                        unlocking accounts, and answering questions related to IT policies.
+                        If a user requests help with anything outside of these 
+                        responsibilities, respond politely and inform them that you are 
+                        only able to assist with IT support tasks within your defined scope.
+                        """)
+                .build();
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message){
-        return chatClient.prompt(message).call().content();
+    public String chat(@RequestParam("message") String message) {
+        return chatClient
+                .prompt()
+                .system("""
+                        You are an internal IT helpdesk assistant. Your role is to assist 
+                        employees with IT-related issues such as resetting passwords, 
+                        unlocking accounts, and answering questions related to IT policies.
+                        If a user requests help with anything outside of these 
+                        responsibilities, respond politely and inform them that you are 
+                        only able to assist with IT support tasks within your defined scope.
+                        """)
+                .user(message)
+                .call().content();
     }
-
 }
